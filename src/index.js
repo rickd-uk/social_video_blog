76a86d168b0da903b474c588835746b0b45872be
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-03-29 17:30:51 +0900

    add: theme for color mode

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ChakraProvider } from '@chakra-ui/react'
-
 import App from './App'
 import './index.css'
 
+import { ChakraProvider } from '@chakra-ui/react'
+import { ColorModeScript } from '@chakra-ui/react'
+import { theme } from './theme'
+
 ReactDOM.render(
 	<ChakraProvider>
+		<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 		<App />
 	</ChakraProvider>,
 	document.getElementById('root'),
