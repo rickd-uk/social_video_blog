4bba01611602e80325ced397cb59c77d5c355581
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-03-29 20:34:41 +0900

    add: Home & Login routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import Home from './Containers/Home'
+import Login from './Containers/Login'
 
 const App = () => {
-	return <h1>APP</h1>
+	return (
+		<Routes>
+			<Route path='login' element={<Login />} />
+			<Route path='/*' element={<Home />} />
+		</Routes>
+	)
 }
 
 export default App
