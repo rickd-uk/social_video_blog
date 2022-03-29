cc82e9776fcf376943b28f7fd7ca03c1048ec346
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-03-29 21:49:35 +0900

    setup: Google signin method

--- src/Containers/Login.jsx
+++ src/Containers/Login.jsx
@@ -1,7 +1,47 @@
+import { Flex, Image, Button, HStack } from '@chakra-ui/react'
 import React from 'react'
 
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { firebaseApp } from '../firebase-config'
+
+import MusicBg from '../img/musicbg.jpg'
+import { FcGoogle } from 'react-icons/fc'
+import { useNavigate } from 'react-router-dom'
+
 const Login = () => {
-	return <div>Login</div>
+	const firebaseAuth = getAuth(firebaseApp)
+	const provider = new GoogleAuthProvider()
+
+	const navigate = useNavigate()
+
+	const login = async () => {
+		const { user } = await signInWithPopup(firebaseAuth, provider)
+		const { refreshToken, providerData } = user
+
+		localStorage.setItem('user', JSON.stringify(providerData))
+		localStorage.setItem('accessToken', JSON.stringify(refreshToken))
+	}
+
+	return (
+		<Flex justifyContent={'center'} alignItems={'center'} width={'100vw'} height={'100vh'} position={'relative'}>
+			<Image src={MusicBg} objectFit='cover' width={'full'} height={'full'} />
+			<Flex
+				position={'absolute'}
+				width={'100vw'}
+				height={'100vh'}
+				bg={'blackAlpha.600'}
+				top={0}
+				left={0}
+				justifyContent='center'
+				alignItems={'center'}>
+				<HStack>
+					<Button leftIcon={<FcGoogle fontSize={25} />} colorScheme='whiteAlpha' shadow={'lg'} onClick={() => login()}>
+						Sign in with Google
+					</Button>
+				</HStack>
+			</Flex>
+		</Flex>
+	)
 }
 
 export default Login
