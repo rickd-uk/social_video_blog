907640f0bdd4809b90ec347e67297d8517119120
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-03-30 18:01:02 +0900

    handles deleting of video

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -51,6 +51,17 @@ const Create = () => {
 		)
 	}
 
+	const deleteImage = () => {
+		const deleteRef = ref(storage, videoAsset)
+		deleteObject(deleteRef)
+			.then(() => {
+				setVideoAsset(null)
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}
+
 	useEffect(() => {
 		console.log(videoAsset)
 	}, [videoAsset])
@@ -170,8 +181,7 @@ const Create = () => {
 								position={'absolute'}
 								cursor={'pointer'}
 								zIndex={10}
-								// onClick={deleteImage}
-							>
+								onClick={deleteImage}>
 								<IoTrash fontSize={20} color='white' />
 							</Flex>
 
