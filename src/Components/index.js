d72ba4f7937621ca9f584602352860f0a311f107
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-03-30 10:00:00 +0900

    add: Category, Create, Feed, Navbar, Search & VideoPin routes and components

---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+export { default as Create } from './Create'
+export { default as Feed } from './Feed'
+export { default as Navbar } from './Navbar'
+export { default as Search } from './Search'
+export { default as VideoPin } from './VideoPin'
+export { default as Category } from './Category'
--- src/Containers/Home.jsx
+++ src/Containers/Home.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Flex } from '@chakra-ui/react'
+import { Category, Create, Feed, Navbar, Search, VideoPin } from '../Components'
+import { Routes, Route } from 'react-router-dom'
 
 const Home = ({ user }) => {
 	console.log(user)
-	return <div>Home</div>
+	return (
+		<>
+			<Navbar user={user} />
+
+			<Flex direction={'column'} justifyContent='start' alignItems={'center'} width='20'>
+				<Category />
+			</Flex>
+
+			<Flex width={'full'} justifyContent='center' alignItems={'center'} px='4'>
+				<Routes>
+					<Route path='/' element={<Feed />} />
+					<Route path='/category/:categoryId' element={<Feed />} />
+					<Route path='/create' element={<Create />} />
+					<Route path='/videoDetail/:videoId' element={<VideoPin />} />
+					<Route path='/search' element={<Search />} />
+				</Routes>
+			</Flex>
+		</>
+	)
 }
 
 export default Home
