905e2d9dd53cb3b8227c46b7ff81255322d842df
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-03-30 12:55:02 +0900

    fix: add flex box as parent in Home; adjust spacing for video section

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -1,7 +1,13 @@
+// prettier-ignore
+import { Flex, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 
 const Create = () => {
-	return <div>Create</div>
+	const { colorMode } = useColorMode()
+	const bg = useColorModeValue('gray.50', 'gray.900')
+	const textColor = useColorModeValue('gray.900', 'gray.50')
+
+	return <Flex justifyContent={'center'} alignItems={'center'} width={'90%'} minHeight='100vh' bg={bg}></Flex>
 }
 
 export default Create
--- src/Containers/Home.jsx
+++ src/Containers/Home.jsx
@@ -9,18 +9,19 @@ const Home = ({ user }) => {
 	return (
 		<>
 			<Navbar user={user} />
-			<Flex direction={'column'} justifyContent='start' alignItems={'center'} width='20'>
-				{categories && categories.map((data) => <Category key={data.id} data={data} />)}
-			</Flex>
-
-			<Flex width={'full'} justifyContent='center' alignItems={'center'} px='4'>
-				<Routes>
-					<Route path='/' element={<Feed />} />
-					<Route path='/category/:categoryId' element={<Feed />} />
-					<Route path='/create' element={<Create />} />
-					<Route path='/videoDetail/:videoId' element={<VideoPin />} />
-					<Route path='/search' element={<Search />} />
-				</Routes>
+			<Flex width={'100vw'}>
+				<Flex direction={'column'} justifyContent='start' alignItems={'center'} width='8%'>
+					{categories && categories.map((data) => <Category key={data.id} data={data} />)}
+				</Flex>
+				<Flex width={'95%'} px={4}>
+					<Routes>
+						<Route path='/' element={<Feed />} />
+						<Route path='/category/:categoryId' element={<Feed />} />
+						<Route path='/create' element={<Create />} />
+						<Route path='/videoDetail/:videoId' element={<VideoPin />} />
+						<Route path='/search' element={<Search />} />
+					</Routes>
+				</Flex>
 			</Flex>
 		</>
 	)
