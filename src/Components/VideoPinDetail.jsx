ac08d877931bc74369e73f22638dc4b823222ab3
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-04-01 15:59:28 +0900

    add: controls on bottom -> goback10, goforward10, play, mute vol control dummy time & logo

--- src/Components/VideoPinDetail.jsx
+++ src/Components/VideoPinDetail.jsx
@@ -1,4 +1,16 @@
-import { Box, Flex, Grid, GridItem, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Text, useColorModeValue } from '@chakra-ui/react'
+import {
+	Box,
+	Flex,
+	Grid,
+	GridItem,
+	Image,
+	Slider,
+	SliderFilledTrack,
+	SliderThumb,
+	SliderTrack,
+	Text,
+	useColorModeValue,
+} from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
@@ -7,6 +19,7 @@ import Spinner from './Spinner'
 
 import { getSpecificVideo } from '../utils/getData'
 
+import logo from '../img/logo.png'
 import { MdForward10, MdFullscreen, MdOutlineReplay10, MdVolumeOff, MdVolumeUp } from 'react-icons/md'
 
 import ReactPlayer from 'react-player'
@@ -78,6 +91,71 @@ const VideoPinDetail = () => {
 								width='full'
 								height='full'>
 								{!isPlaying && <IoPlay fontSize={60} color='#f2f2f2' cursor={'pointer'} />}
+							</Flex>
+
+							{/* Progress Controls */}
+							<Flex
+								width={'full'}
+								alignItems='center'
+								direction={'column'}
+								px={4}
+								bgGradient='linear(to-t, blackAlpha.900, blackAlpha.500, blackAlpha.50)'>
+								{/* Slider */}
+								<Slider aria-label='slider-ex-4' defaultValue={30} min={0} max={100}>
+									<SliderTrack bg='teal.50'>
+										<SliderFilledTrack bg='teal.300' />
+									</SliderTrack>
+									<SliderThumb boxSize={3} bg='teal.300' />
+								</Slider>
+
+								{/* Other player controls */}
+								<Flex width={'full'} alignItems={'center'} my={2} gap={10}>
+									<MdOutlineReplay10 fontSize={30} color={'#f1f1f1'} cursor={'pointer'} />
+									<Box onClick={() => setIsPlaying(!isPlaying)}>
+										{!isPlaying ? (
+											<IoPlay fontSize={30} color='#f2f2f2' cursor={'pointer'} />
+										) : (
+											<IoPause fontSize={30} color='#f2f2f2' cursor={'pointer'} />
+										)}
+									</Box>
+									<MdForward10 fontSize={30} color={'#f1f1f1'} cursor={'pointer'} />
+									{/* Volumes controls */}
+									<Flex alignItems={'center'}>
+										<Box
+											onClick={() => {
+												setMuted(!muted)
+											}}>
+											{!muted ? (
+												<MdVolumeUp fontSize={30} color='#f1f1f1' cursor='pointer' />
+											) : (
+												<MdVolumeOff fontSize={30} color='#f1f1f1' cursor='pointer' />
+											)}
+										</Box>
+
+										<Slider aria-label='slider-ex-1' defaultValue={30} min={0} max={100} size={'sm'} width={26} mx={2}>
+											<SliderTrack bg='teal.50'>
+												<SliderFilledTrack bg='teal.300' />
+											</SliderTrack>
+											<SliderThumb boxSize={3} bg='teal.300' />
+										</Slider>
+									</Flex>
+									{/* EO Volume Controls */}
+									{/* Duration of video */}
+									<Flex alignItems={'center'} gap={2}>
+										<Text fontSize={16} color='whitesmoke'>
+											0:00
+										</Text>
+										<Text fontSize={16} color='whitesmoke'>
+											/
+										</Text>
+										<Text fontSize={16} color='whitesmoke'>
+											0:00
+										</Text>
+									</Flex>
+									<Image src={logo} width={'120px'} ml={'auto'} />
+
+									<MdFullscreen fontSize={30} color='#f1f1f1' cursor={'pointer'} onClick={() => {}} />
+								</Flex>
 							</Flex>
 						</Flex>
 					</Flex>
